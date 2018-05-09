e30c0d260d5392102d6ad6c8c0ac809c53593318
Camile Lindsey <camille@aliis-MacBook-Pro.local>
2018-05-08 20:18:27 -1000

    array

--- exercise.js
+++ exercise.js
@@ -13,6 +13,13 @@
 /*1. Mr. Bonner (aka the Red Rocket, the Red Mamba) doesn't belong in this class of Hall Of Famers. Remove this imposter immediately and assign it to a new variable named `notHof` Console.log this new variable as well as the original list.*/ 
 
 var hof = ["Jordan", "Bird", "Kobe", "Duncan", "Iverson", "Bonner"];
+var notHof = hof.pop(); 
+console.log(notHof);
+console.log(hof);
+
+
+
+
 
 
 
@@ -21,31 +28,39 @@ var hof = ["Jordan", "Bird", "Kobe", "Duncan", "Iverson", "Bonner"];
 /*2. You've been tasked to do this week's grocery shopping. As you arrive at Times Supermarket, Kanye pings you to get a bottle of Hendricks gin. Add this to the existing shoppingList and console.log it.*/
 
 var shoppingList = ["cool ranch doritos", "kings hawaiian sweet bread", "peanut butter oreos", "fruit loops cereal"];
-
+shoppingList.push('hendricks');
+console.log(shoppingList)
 
 
 
 /*The array.reverse() reverses the order of the element of an array in place - the first becomes the last and the last becomes the first*/
+var shoppingList2 = ["cool ranch doritos", "kings hawaiian sweet bread", "peanut butter oreos", "fruit loops cereal"];
+shoppingList2.reverse()
+console.log(shoppingList2)
+
 
 /*3. Use the force, or in this case the reverse method to help Yoda make some sense with his motivational talk to the young Jedi interns.*/
 
-var yoda = ["try", "no", "is", "there", "not", "do", "or", "do"];
-
-
+var yodaTalk = ["try", "no", "is", "there", "not", "do", "or", "do"];
+yodaTalk.reverse();
+console.log(yodaTalk)
 
 /*The array.shift() removes the first element from an array and returns that element*/
 
 /*4. People been lining up for hours to get the newest iphone release. Help manage the unruly crowd of privileged customers by serving them one at a time and assiging it to a variable named `nowServing`. Console.log this new variable as well as the waitList.*/
 
 var waitList = ["Chance the Rapper", "Khalid", "Tay-Tay", "Barry Manilow", "Piko Taro"];
-
-
+waitList.shift()
+console.log(waitList)
+var nowServing = waitList.shift()
+console.log(nowServing)
 
 /*The array.unshift() adds one or more elements to the front of an array and returns the new length of the array*/
 
 /*5. Apparently, you didn't do a good job with crowd control in the previous exercise. Kanye just cut in front of everyone!. Add Kanye to the waitList array above. Console.log waitList*/
-
-
+var waitList2 = ["Chance the Rapper", "Khalid", "Tay-Tay", "Barry Manilow", "Piko Taro"];
+waitList2.unshift ("Kanye")
+console.log(waitList2)
 
 /*The array.splice() adds and/or removes elements from an array.
 array.splice(index, how many items to be removed)*/
@@ -53,8 +68,8 @@ array.splice(index, how many items to be removed)*/
 /*6. As head of McDonald's New Product Division, your job is to test and approve all new menu items. In the testMenu array below, all the items are approved except for "McLinguine" and "McChar-Siu Bau". Remove these items from the list and console.log the updated testMenu.*/
 
 var testMenu = ["McPizza Burger", "McFly Burger", "McLinguine", "McChar-Siu Bau", "McChop-Chae", "McPancit"];
-
-
+testMenu.splice(2, 2)
+console.log(testMenu)
 
 /*=============================================================*/
 
@@ -66,16 +81,17 @@ var testMenu = ["McPizza Burger", "McFly Burger", "McLinguine", "McChar-Siu Bau"
 
 var westSide = ["Dre", "2Pac", "Snoop"];
 var eastSide = ["Biggie", "Diddy", "Craig Mack"];
-
-
+var oneLove = westSide.concat(eastSide);
+console.log(oneLove)
 
 /*The array.join() joins all the elements of an array into a string.*/
 
 /*8. Help Nike sell more overpriced sneakers by changing their slogan from an array into a string. Convert the shoe array and assign it to a variable named `shoeString`. Console.log this new variable.*/
 
 var shoe = ["just", "do", "it"];
-
-
+//shoe.join(" ")
+var shoeString = shoe.join(" ");
+console.log(shoeString);
 
 /*The array.slice() extracts a section of an array and returns a new array.
 array.slice(index start, index end)*/
@@ -84,8 +100,6 @@ array.slice(index start, index end)*/
 "Yeezy", "Kimmy K" and "Lamar Odom". Unfriend these ungrateful people and assign them to a new variable named `unFriend`. Console.log this new variable.*/
 
 var fbFriends = ["Khalid", "Yeezy", "Kimmy K", "Lamar Odom", "Lebron", "Warren Buffet"];
-
-
-
-
+var unFriend = (fbFriends.slice( 1, 4 ))
+console.log(unFriend)
 
